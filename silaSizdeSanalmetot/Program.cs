fb89cbf3e24d74fb17a0913c1083c2d4a32d0a3c
eren595 <eren595@BPC12>
2023-12-26 11:17:05 +0300

    Proje dosyası ekle.

---
+++ silaSizdeSanalmetot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace silaSizdeSanalmetot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap kitap = new Kitap();
+            kitap.Oku();
+
+            Dergi dergi = new Dergi();
+            dergi.Oku();
+
+            Ansiklopedi ansiklopedi = new Ansiklopedi();
+            ansiklopedi.Oku();
+        }
+    }
+    public interface Okuyabilir
+    {
+        void Oku();
+    }
+    public class Kitap : Okuyabilir
+    {
+        public void Oku()
+        {
+            Console.WriteLine("Kitap okunuyor...");
+        }
+    }
+    public class Dergi : Okuyabilir
+    {
+        public void Oku()
+        {
+            Console.WriteLine("Dergi okunuyor...");
+
+        }
+    }
+    public class Ansiklopedi : Okuyabilir
+    {
+        public void Oku()
+        {
+            Console.WriteLine("Ansiklopedi okunuyor...");
+        }
+    }
+}
